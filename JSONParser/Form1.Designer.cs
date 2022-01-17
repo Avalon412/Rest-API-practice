2d1d68cf8a1cbf7d0bb26c0d0cfd2e8f12c487df
Женя Василинчук <marakasi88887@gmail.com>
2022-01-17 03:32:11 +0200

    Added JSON Decerializer

--- JSONParser/Form1.Designer.cs
+++ JSONParser/Form1.Designer.cs
@@ -33,39 +33,49 @@ namespace JSONParser
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.btnDeserialise = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtJSON
             // 
-            this.txtJSON.Location = new System.Drawing.Point(12, 12);
+            this.txtJSON.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJSON.Location = new System.Drawing.Point(6, 19);
             this.txtJSON.Multiline = true;
             this.txtJSON.Name = "txtJSON";
             this.txtJSON.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtJSON.Size = new System.Drawing.Size(710, 166);
+            this.txtJSON.Size = new System.Drawing.Size(698, 141);
             this.txtJSON.TabIndex = 0;
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(12, 216);
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(6, 19);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.ReadOnly = true;
             this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.Size = new System.Drawing.Size(710, 166);
+            this.txtOutput.Size = new System.Drawing.Size(692, 141);
             this.txtOutput.TabIndex = 1;
             // 
             // btnDeserialise
             // 
-            this.btnDeserialise.Location = new System.Drawing.Point(12, 184);
+            this.btnDeserialise.Location = new System.Drawing.Point(18, 184);
             this.btnDeserialise.Name = "btnDeserialise";
             this.btnDeserialise.Size = new System.Drawing.Size(127, 26);
             this.btnDeserialise.TabIndex = 2;
             this.btnDeserialise.Text = "Deserialise";
             this.btnDeserialise.UseVisualStyleBackColor = true;
+            this.btnDeserialise.Click += new System.EventHandler(this.btnDeserialise_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(145, 184);
+            this.btnClear.Location = new System.Drawing.Point(151, 184);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(127, 26);
             this.btnClear.TabIndex = 3;
@@ -73,19 +83,47 @@ namespace JSONParser
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtJSON);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(710, 166);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Raw JSON (Serialised String)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Location = new System.Drawing.Point(12, 216);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(704, 166);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Debug Output";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 394);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnDeserialise);
-            this.Controls.Add(this.txtOutput);
-            this.Controls.Add(this.txtJSON);
+            this.Controls.Add(this.groupBox2);
             this.Name = "Form1";
             this.Text = "C# JSON Parser";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,6 +133,8 @@ namespace JSONParser
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.Button btnDeserialise;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- JSONParser/Form1.cs
+++ JSONParser/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using JSONParser.Models;
 
 namespace JSONParser
 {
@@ -17,9 +12,62 @@ namespace JSONParser
             InitializeComponent();
         }
 
+        #region UI Events
+
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtOutput.Text = string.Empty;
         }
+
+        private void btnDeserialise_Click(object sender, EventArgs e)
+        {
+            //DebugOutput(txtJSON.Text);
+            DecerialiseJSON(txtJSON.Text);
+        }
+
+        #endregion
+
+        #region JSON Functions
+
+        private void DecerialiseJSON(string strJSON)
+        {
+            try
+            {
+                var jPerson = JsonConvert.DeserializeObject<Person>(strJSON);
+                DebugOutput("Here`s our JSON object: " + jPerson.ToString());
+                DebugOutput("Here`s our Person FirstName: " + jPerson.FirstName.ToString());
+
+                //var jPerson = JsonConvert.DeserializeObject<dynamic>(strJSON);
+                //DebugOutput("Here`s our JSON object: " + jPerson.ToString());
+
+                //DebugOutput("Here`s the First Name: " + jPerson.firstname);
+                //DebugOutput("Here`s the Street Address: " + jPerson.streetaddress.house);
+            }
+            catch(Exception ex)
+            {
+                DebugOutput("We had a problem: " + ex.Message.ToString());
+            }
+        }
+
+        #endregion
+
+        #region Debug Output
+
+        private void DebugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtOutput.Text = txtOutput.Text + strDebugText + Environment.NewLine;
+                txtOutput.SelectionStart = txtOutput.TextLength;
+                txtOutput.ScrollToCaret();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message.ToString() + Environment.NewLine);
+            }
+        }
+
+        #endregion
     }
 }
